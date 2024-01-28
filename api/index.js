ba0d07e80247228c30c8a8d6c2886b01a2e8dc92
Alexander Obozny <sasha.obozny@gmail.com>
2024-01-28 20:08:04 +0200

    singup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('server run1');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
